270645159f3e288f9700ffad466faa23b2a24df5
Shahid <shahid.sarker@gmail.com>
2019-07-15 20:39:54 -0400

    operate fail

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,7 +2,9 @@ import Big from "big.js";
 
 export default function operate(numberOne, numberTwo, operation) {
   const one = Big(numberOne || "0");
-  const two = Big(numberTwo || (operation === "÷" || operation === 'x' ? "1": "0")); //If dividing or multiplying, then 1 maintains current value in cases of null
+  const two = Big(
+    numberTwo || (operation === "÷" || operation === "x" ? "1" : "0"),
+  ); //If dividing or multiplying, then 1 maintains current value in cases of null
   if (operation === "+") {
     return one.plus(two).toString();
   }
@@ -10,7 +12,7 @@ export default function operate(numberOne, numberTwo, operation) {
     return one.minus(two).toString();
   }
   if (operation === "x") {
-    return one.times(two).toString();
+    return one.times(one).toString();
   }
   if (operation === "÷") {
     if (two === "0") {
